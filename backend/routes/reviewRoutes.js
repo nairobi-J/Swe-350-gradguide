fef07c4693d069c2894fb6f5aebcd0a36053b7b9
abusayeid11 <abusayeidsawon@gmail.com>
2025-07-27 20:55:10 +0600

    review routes updated

---
+++ backend/routes/reviewRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const reviewController = require('../controllers/reviewController')
+
+router.get('/avg-review', reviewController.getAvgReview)
+router.get('/review-by-firm', reviewController.getReviewByFirm)
+
+module.exports = router
